789d76c33fc79cd24350b1b4f1124f46af3376e2
LorenzoNog <lnogueramartinez01@gmail.com>
2023-07-19 15:12:06 -0300

    Actualizando cotizaciones

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 
 interface formProps {
-    value: number,
-    onChange: (amount : number) => void
+  value: number;
+  onChange: (amount: number) => void;
 }
 
-const Form = ({value, onChange}: formProps) => {
-
+const Form = ({ value, onChange }: formProps) => {
   return (
     <form className="w-full flex flex-col">
-        <label>Monto en ARS</label>
-        <input
+      <label>Monto en ARS</label>
+      <input
         className="p-2 text-right border-2 rounded-full mt-2"
+        id="input"
         type="number"
-        value={value}
-        onChange={e => onChange(Number(e.target.value))}
+        value={ value === 0 ? ' ' : value}
+        onChange={(e) => onChange(Number(e.target.value))}
       />
     </form>
   );
